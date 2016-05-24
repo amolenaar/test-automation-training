9b157ddf3385111052a0f4286e92095943ed34d7
Arjan Molenaar <gaphor@gmail.com>
2016-05-24 11:31:39 +0200

    Update fitnesse example for web test.

--- src/test/java/com/xebia/tatraining/fitnesse/FreeShipping.java
+++ src/test/java/com/xebia/tatraining/fitnesse/FreeShipping.java
@@ -1,10 +1,27 @@
 package com.xebia.tatraining.fitnesse;
 
-import com.xebia.tatraining.basket.Basket;
-import com.xebia.tatraining.catalog.Category;
-import com.xebia.tatraining.catalog.Price;
-import com.xebia.tatraining.catalog.Product;
+import java.util.List;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FreeShipping {
+    private FirefoxDriver driver;
+
+    public void beginTable() {
+        driver = new FirefoxDriver();
+        driver.get("http://localhost:8000");
+    }
+
+    public void endTable() {
+        driver.quit();
+    }
+
+    public void reset() {
+        driver.navigate().refresh();
+    }
 
 }
